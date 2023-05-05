77d2c36ad241af1b6ebdbc5c93c49f370917350c
bedranozcan <bedra@IDVLABS>
2023-05-05 16:12:45 +0300

    Identity Kütüphanesi eklendi ve sınıflar oluşturulmaya başlandı.

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=idvlabs;initial catalog=EasyCashDb;integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+        public int CustomerAccountID { get; set; }
+        public CustomerAccount CustomerAccount { get; set; }
+    }
+}
