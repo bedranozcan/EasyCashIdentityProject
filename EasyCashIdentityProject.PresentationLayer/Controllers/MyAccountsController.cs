742fe83af1a82988acdc30f9963462fb8aeb4267
bedranozcan <bedra@IDVLABS>
2023-08-23 13:05:32 +0300

    Giriş Yapan Müşterinin Profil Bilgilerini Güncelleme

--- EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -29,5 +29,32 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
             appUserEditDto.ImageUrl=values.ImageUrl;
             return View(appUserEditDto);
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(AppUserEditDto appUserEditDto)
+        {
+            if (appUserEditDto.Password == appUserEditDto.ConfirmPassword)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                user.Name = appUserEditDto.Name;
+                user.Surname = appUserEditDto.Surname;
+                user.PhoneNumber = appUserEditDto.PhoneNumber;
+                user.City = appUserEditDto.City;
+                user.District = appUserEditDto.District;
+                user.ImageUrl = "test";
+                user.Email = appUserEditDto.Email;
+                user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, appUserEditDto.Password);
+
+                var result=await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Login");
+                }            
+                                
+            }
+            return View();
+        }
+
     }
 }
